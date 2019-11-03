18905923debd2b62c4ada7b939cefa0283d07cd0
TankadiN <bartek12tuzik@gmail.com>
2019-11-03 17:59:25 +0100

    PlayerController work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,53 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
-    public static Vector2 Position;
+    [Header("Game Objects")]
+    public Slider bar;
+    public Image soul;
+    public TMP_Text hpTextbox;
+    public TMP_Text lvlTextbox;
+    public TMP_Text expTextbox;
+    [Header("Health Variables")]
+    public float maxHealth;
+    public float currentHealth;
+    [Header("Level Variables")]
+    public float level;
+    public float maxExperience;
+    public float currentExperience;
 
-    public float moveSpeed = 5f;
-    public float moveSmooth = .3f;
-    public bool aimAtCursor;
-
-    private Rigidbody2D rb;
-
-    Vector2 movement = Vector2.zero;
-    Vector2 velocity = Vector2.zero;
-
-    Vector2 mousePos = Vector2.zero;
-
-    // Use this for initialization
-    void Start()
+    void Start ()
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
+        currentHealth = maxHealth;
+	}
+	
+	void Update ()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-    }
-
-    private void FixedUpdate()
-    {
-
-        Vector2 desiredVelocity = movement * moveSpeed;
-        rb.velocity = Vector2.SmoothDamp(rb.velocity, desiredVelocity, ref velocity, moveSmooth);
-
-        Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        if(aimAtCursor)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            rb.rotation = angle + 180f;
+            currentHealth--;
         }
+        if (Input.GetKey(KeyCode.E))
+        {
+            currentExperience++;
+        }
+        if (currentExperience >= maxExperience)
+        {
+            LevelUp();
+        }
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //Display Logic
+        string sMaxHp = maxHealth.ToString();
+        string sCurHp = currentHealth.ToString();
+        string sLvl = maxHealth.ToString();
+        string sMaxExp = maxExperience.ToString();
+        string sCurExp = currentExperience.ToString();
 
-        Position = rb.position;
+        hpTextbox.text = sCurHp + "/" + sMaxHp;
+        lvlTextbox.text = "lv " + level;
+        expTextbox.text = "exp " + sCurExp + "/" + sMaxExp;
+
+        bar.maxValue = maxHealth;
+        bar.value = currentHealth;
+        soul.fillAmount = currentExperience / maxExperience;
     }
 
+    public void LevelUp()
+    {
+        AudioManager.instance.Play("levelup");
+        bar.GetComponent<RectTransform>().sizeDelta += new Vector2(25f, 0f);
+        level++;
+        maxHealth += 4f;
+        currentHealth = maxHealth;
+        maxExperience += 50f;
+        currentExperience = 0f;
+    }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public static Vector2 Position;
+
+    public float moveSpeed = 5f;
+    public float moveSmooth = .3f;
+    public bool aimAtCursor;
+
+    private Rigidbody2D rb;
+
+    Vector2 movement = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
+
+    Vector2 mousePos = Vector2.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector2 desiredVelocity = movement * moveSpeed;
+        rb.velocity = Vector2.SmoothDamp(rb.velocity, desiredVelocity, ref velocity, moveSmooth);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        if(aimAtCursor)
+        {
+            rb.rotation = angle + 180f;
+        }
+
+        Position = rb.position;
+    }
+
+}
