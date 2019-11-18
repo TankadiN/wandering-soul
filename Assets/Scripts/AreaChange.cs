65d5b8956a37c3c7bd1a95aa7e106a2c671aeacb
TankadiN <bartek12tuzik@gmail.com>
2019-11-18 16:58:42 +0100

    Implemented Area Transitions

---
+++ Assets/Scripts/AreaChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaChange : MonoBehaviour
+{
+    public static AreaChange instance;
+    public GameObject activeCamera;
+    public GameObject destinatedCamera;
+    public GameObject locationTeleport;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GameObject.Find("Canvas").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            StartCoroutine(ChangeArea());
+        }
+    }
+
+    public IEnumerator ChangeArea()
+    {
+        anim.SetTrigger("FadeOut");
+        yield return new WaitForSeconds(0.5f);
+        activeCamera.SetActive(false);
+        destinatedCamera.SetActive(true);
+        GameObject.Find("Player").transform.position = locationTeleport.transform.position;
+        anim.SetTrigger("FadeIn");
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
