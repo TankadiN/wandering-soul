b0578a5f0429e560558dcec4bc62ad5cfffaafc7
TankadiN <bartek12tuzik@gmail.com>
2019-11-21 17:03:12 +0100

    Button Navigation Work

---
+++ Assets/Scripts/ButtonExtension.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonExtension : MonoBehaviour
+{
+    private Button myButton;
+    public bool toPressedColor;
+    public List<Image> Images;
+    public List<TMP_Text> Texts;
+
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        if(myButton.interactable == false)
+        {
+            for(int i = 0; i <= Images.Count - 1; i++)
+            {
+                if(toPressedColor)
+                {
+                    Images[i].color = myButton.colors.pressedColor;
+                }
+                else
+                {
+                    Images[i].color = myButton.colors.highlightedColor;
+                }
+            }
+            for (int i = 0; i <= Texts.Count - 1; i++)
+            {
+                Texts[i].color = myButton.colors.pressedColor;
+            }
+        }
+        else
+        {
+            for (int i = 0; i <= Images.Count - 1; i++)
+            {
+                Images[i].color = myButton.colors.normalColor;
+            }
+            for (int i = 0; i <= Texts.Count - 1; i++)
+            {
+                Texts[i].color = myButton.colors.highlightedColor;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MenuInput.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuInput : MonoBehaviour
+{
+    public Button[] buttons;
+    public GameObject FirstSelect;
+    public GameObject Selected;
+
+    void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(FirstSelect);
+    }
+
+    void Update()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject);
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(Selected);
+        }
+        if (EventSystem.current.currentSelectedGameObject.GetComponent<Button>().interactable == false)
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                if(buttons[i].interactable)
+                {
+                    EventSystem.current.SetSelectedGameObject(buttons[i].gameObject);
+                    break;
+                }
+            }
+            Debug.Log(EventSystem.current.currentSelectedGameObject + " is NOT active!");
+        }
+        Selected = EventSystem.current.currentSelectedGameObject;
+    }
+}
